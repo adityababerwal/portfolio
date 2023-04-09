1818adfbd602774d3a87cc0fc8b6e10df41f2b70
adityababerwal <adityababerwal@gmail.com>
2023-04-09 19:39:40 +0530

    fix a simple WARN

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import './components/Header/Header';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import Projects from './components/Projects/Projects';
 
 function App() {
   return (
